8b7f7ab12400175c96f13e21620a3c525daac83d
jugipe <jukka.pelli@tuni.fi>
2023-01-02 12:31:45 +0200

    update depencies and initialize database on api start

--- server/src/index.js
+++ server/src/index.js
@@ -1,6 +1,7 @@
 require("dotenv").config({ path: "../.env" })
 const express = require("express");
 const cors = require("cors")
+const db = require("./db/db")
 
 const app = express();
 const port =  process.env.API_PORT || 9001;
@@ -9,4 +10,8 @@ const port =  process.env.API_PORT || 9001;
 app.use(cors());
 app.use(express.json());
 
-app.listen(port, () => {console.log("helcitybiker is running @ "+port)});
+db.init().then(() => {
+    app.listen(port, () => {console.log("helcitybiker is running @ "+port)});
+}).catch((err) => {
+    console.log(err);
+});
