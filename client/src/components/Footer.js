ce28c55a7e76fa6b2f38183c1cabe26a03fb5b8e
jugipe <jukka.pelli@tuni.fi>
2023-01-04 14:00:32 +0200

    Add footer react component

---
+++ client/src/components/Footer.js
@@ -0,0 +1,10 @@
+const Footer = () => {
+    return (
+        <footer>
+            <p className="small">&copy; <a href="https://github.com/jugipe">jugipe</a> 2023</p>
+            <p className="small"><a href="mailto:jukka.pelli@tuni.fi">feedback</a></p>
+        </footer>
+    )
+}
+
+export default Footer;
