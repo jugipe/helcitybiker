26dd0e2c012a345ea12e1d18a85edbd31ab1aa24
jugipe <jukka.pelli@tuni.fi>
2023-01-04 11:48:28 +0200

    Modify getting station name with useParams hook instead of parsing from window location

--- client/src/components/StationInfo.js
+++ client/src/components/StationInfo.js
@@ -3,17 +3,17 @@ import { useParams } from "react-router-dom";
 
 const StationInfo = () => {
 
-    const loc = window.location.pathname;
-    const stationName = loc.split("/")[2];
+    // Get station name with useParams hook
+    const { name } = useParams();
 
     const [ station, setStation ] = useState({});
 
     useEffect(() => {
-        fetch("http://localhost:9001/stations/"+stationName)
+        fetch("http://localhost:9001/stations/"+name)
             .then(data => data.json())
             .then(array => array[0]) // take the first of the array
             .then(setStation)
-    }, [stationName]);
+    }, [name]);
 
     if(station === undefined || station.name === undefined){return <h1>No Stationdata with Given Station Name</h1>}
 
