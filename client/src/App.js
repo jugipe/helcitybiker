9cc7aeb18ef5f37ca6ca5c95fbcb79c76676eb9c
jugipe <jukka.pelli@tuni.fi>
2023-01-04 14:38:20 +0200

    Add NavBar & Footer to layout

--- client/src/App.js
+++ client/src/App.js
@@ -5,16 +5,22 @@ import './App.css';
 import JourneyList from "./components/JourneyList";
 import StationsList from "./components/StationsList";
 import StationInfo from "./components/StationInfo";
+import NavBar from "./components/NavBar";
+import NoPage from "./components/NoPage";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <Router>
       <div className="App">
+        <NavBar />
         <Routes>
             <Route exact path="/" element={<JourneyList />}></Route>
             <Route exact path="/stations" element={<StationsList />}></Route>
             <Route exact path="/stationinfo/:name" element={<StationInfo />}></Route>
+            <Route exact path="*" element={<NoPage />}></Route>
         </Routes>
+        <Footer />
       </div>
     </Router>
   );
