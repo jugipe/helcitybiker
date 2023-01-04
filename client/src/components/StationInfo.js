33b696996deb70648da48d7366e5e4b54516620d
jugipe <jukka.pelli@tuni.fi>
2023-01-04 15:36:51 +0200

    Add more info about the station to StationInfo

--- client/src/components/StationInfo.js
+++ client/src/components/StationInfo.js
@@ -18,7 +18,20 @@ const StationInfo = () => {
     if(station === undefined || station.name === undefined){return <h1 className="text-center mt-3">No Stationdata with Given Station Name</h1>}
 
     return (
-        <h1 className="text-center mt-3">{station.name}</h1>
+        <div>
+            <h1 className="text-center mt-3">{station.name}</h1>
+            <div className="container d-flex flex-row mt-2 d-inline-block flex-row">
+                <div className="stationInfoCard d-flex mt-2 w-50 flex-column">
+                    <h1>{station.osoite}</h1>
+                    <h2>{station.kaupunki}</h2>
+                    <p>placeholder for starting journeys</p>
+                    <p>placeholder for ending journeys</p>
+                </div>
+                <div className="stationInfoCard d-inline-flex p2 w-50 justify-content-center flex-column bg-warning">
+                    <p>Map placeholder</p>
+                </div>
+            </div>
+        </div>
     )
 }
 
