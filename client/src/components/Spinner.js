76203dae9c9567ffd393a67e0d8b32138a329f76
jugipe <jukka.pelli@tuni.fi>
2023-01-05 14:13:34 +0200

    Add loading spinner react component

---
+++ client/src/components/Spinner.js
@@ -0,0 +1,10 @@
+const Spinner = () => {
+  return (
+    <div className="container mt-3 stationInfoCard">
+        <div className="spinner-border">
+        </div>
+    </div>
+  );
+}
+
+export default Spinner;
