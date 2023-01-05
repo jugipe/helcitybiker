c1aaef0e5803122050c0fa04d0b5cc4ebb9b32be
jugipe <jukka.pelli@tuni.fi>
2023-01-05 12:35:47 +0200

    Add unit tests for JourneyList react component

---
+++ client/src/test/JourneyList.test.js
@@ -0,0 +1,53 @@
+import { render, screen, waitFor } from "@testing-library/react";
+import JourneyList from "../components/JourneyList";
+import { getJourneysFromApi } from "../api/getJourneysFromApi";
+import { BrowserRouter } from "react-router-dom";
+
+/**
+ * Mock the api module
+ */
+jest.mock("../api/getJourneysFromApi");
+
+describe("JourneyList Component", () => {
+
+    // After each test clear the mock
+    beforeEach(() => jest.clearAllMocks());
+  
+    it("should render journeys when api responds", async () => {
+
+      getJourneysFromApi.mockResolvedValue([{"id": 1, "departure": "2021-05-31T23:57:25", "return": "2021-06-01T00:05:46",
+       "departure_id":101, "departure_name": "Test1", "return_id": 102, "return_name": "Test2", "distance": 2043, "duration": 500}]);
+
+      // Render the component
+      render(<BrowserRouter><JourneyList/></BrowserRouter>);
+
+      // See if all the content of mocked resolve value is rendered
+      await waitFor(() => {
+        screen.findByText("Test1");
+        screen.findByText("Test2");
+        screen.findByText("2043 m");
+        screen.findByText("500 s");
+      });
+    });
+
+    it("should render error when api returns an empty array", async () => {
+      getJourneysFromApi.mockResolvedValue([]);
+        
+      render(<BrowserRouter><JourneyList/></BrowserRouter>);
+      
+      // See if the error message is rendered
+      await waitFor(() => {
+        screen.findByText("Unable to fetch data");
+      });
+    });
+
+    it("should render error message when api fails", async () => {
+      getJourneysFromApi.mockRejectedValue({});
+
+      async () => render(<BrowserRouter><JourneyList/></BrowserRouter>);
+      
+      await waitFor(() => {
+        screen.findByText("Unable to fetch data");
+      });
+    });
+});
