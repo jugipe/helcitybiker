5a8bfcd48ae9103c421786d538937751437f5f74
jugipe <jukka.pelli@tuni.fi>
2023-01-09 10:51:41 +0200

    Update api endpoint tests according to changes to api logic

--- server/test/endpoint.integration.test.js
+++ server/test/endpoint.integration.test.js
@@ -1,38 +1,42 @@
 const server = require("../src/index.js");
 const chai = require("chai");
 const chaiHttp = require("chai-http");
-const should = chai.should();
 const db = require("../src/db/db")
 const path = require("path");
 const populateDB = require("../src/util/populateDB");
 
+const should = chai.should();
 chai.use(chaiHttp);
 
-describe("Express server", async () =>{
-    before("Load test data", async() => {
-        
+describe("Express server", () =>{
+    before("Wait for server to load", (done) => 
+        server.on("started", done));
+
+
+    before("Load test data", () => {
+   
         // Add data from testdata and add it to test database
-        await populateDB(path.join(__dirname, "/testdata/"));
+        populateDB(path.join(__dirname, "/testdata/"));
         
         console.log("---");
     });
 
-    after("Clear test data", async() => {
-        const arr = ["stations", "journeys"];  
-        await db.truncateTable(arr);
+    after("Clear test data", () => {
+        const arr = ["raw_stations", "raw_journeys"];  
+        db.truncateTable(arr);
     });
 
     const port = process.env.API_PORT || 8000;
     const api = "localhost:"+port;
 
-    describe("/GET stations",  async () => {
+    describe("/GET stations",  () => {
         it("it should have status 200", (done) => {
             chai.request(api)
                 .get("/stations")
                 .end((err, res) => {
                     res.should.have.status(200);
                 done();
-                });
+            });
         });
         it("it should return a json array", (done) => {
             chai.request(api)
@@ -48,18 +52,18 @@ describe("Express server", async () =>{
                 .end((err, res) => {               
                     res.body.length.should.be.equal(5);
                 done();
-                });
+            });
         });
     });
 
-    describe("/GET journeys",  async () => {
+    describe("/GET journeys",  () => {
         it("it should have status 200", (done) => {
             chai.request(api)
                 .get("/journeys")
                 .end((err, res) => {
                     res.should.have.status(200);
                 done();
-                });
+            });
         });
         it("it should return a json array", (done) => {
             chai.request(api)
@@ -75,11 +79,11 @@ describe("Express server", async () =>{
                 .end((err, res) => {
                     res.body.length.should.be.equal(5);
                 done();
-                });
+            });
         });
     });
 
-    describe("/GET a station",  async () => {
+    describe("/GET a station",  () => {
 
         // Get the first station name of known data
         const firstStationName = "Test1"
@@ -90,7 +94,7 @@ describe("Express server", async () =>{
                 .end((err, res) => {
                     res.should.have.status(200);
                 done();
-                });
+            });
         });
         it("it should return a json array", (done) => {
             chai.request(api)
@@ -98,7 +102,7 @@ describe("Express server", async () =>{
                 .end((err, res) => {
                     res.body.should.be.a("array");
                 done();
-                });
+            });
         });
         it("it should GET only one station", (done) => {
             chai.request(api)
@@ -106,7 +110,7 @@ describe("Express server", async () =>{
                 .end((err, res) => {
                     res.body.length.should.be.equal(1);
                 done();
-                });
+            });
         });
         it("it should GET station info", (done) => {
             chai.request(api)
@@ -117,11 +121,11 @@ describe("Express server", async () =>{
                     const data = res.body[0];
                     data.should.include({nimi: firstStationName})
                 done();
-                });
+            });
         });
     });
 
-    describe("/GET a non-existent station",  async () => {
+    describe("/GET a non-existent station",  () => {
         it("it should not GET station info", (done) => {
             chai.request(api)
                 .get("/stations/nonexistingstation")
@@ -130,19 +134,18 @@ describe("Express server", async () =>{
                     res.should.not.be.a("array");
                     res.body.should.be.empty;
                 done();
-                });
+            });
         });
-    
     });
     
-    describe("/GET non existent route",  async () => {
+    describe("/GET non existent route",   () => {
         it("it should return 404", (done) => {
             chai.request(api)
                 .get("/nonexistingroute")
                 .end((err, res) => {
                     res.should.have.status(404);
                     done();
-                });
+            });
         });
     });
 });
