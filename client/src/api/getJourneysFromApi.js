cbad3ac30384bd60379ef24035add4fc72e9b0bc
jugipe <jukka.pelli@tuni.fi>
2023-01-09 16:45:17 +0200

    Add offset / limit to getJourneysFromApi function

--- client/src/api/getJourneysFromApi.js
+++ client/src/api/getJourneysFromApi.js
@@ -1,5 +1,5 @@
-export const getJourneysFromApi = () => {
-    return fetch(process.env.REACT_APP_API_HOST+"/journeys").then((response) => {
+export const getJourneysFromApi = (offset, limit) => {
+    return fetch(process.env.REACT_APP_API_HOST+"/journeys?offset="+offset+"&limit="+limit).then((response) => {
       if (response.status === 200) return response.json();
       else throw new Error("Invalid response");
     });
