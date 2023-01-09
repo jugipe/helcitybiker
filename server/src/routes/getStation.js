edb314e54246672dee21b16c50d5fa2a7fa53123
jugipe <jukka.pelli@tuni.fi>
2023-01-09 13:20:46 +0200

    Update getStation function to get all the information we want to visualize before returning the response

--- server/src/routes/getStation.js
+++ server/src/routes/getStation.js
@@ -2,8 +2,19 @@ const db = require("../db/db");
 
 const getStation = async (req, res) => {
     const name = req.params.name;
-    const station = await db.getStation(name);
-    res.send(station.rows);
+
+    // Make an array of all the data, wait for all promises to resolve before returning
+    // the data.
+    const array = await Promise.all([
+        // get the data and process it to better format
+        db.getStation(name).then(data => data.rows).then(rows => rows[0]),
+        db.getStationDepInfo(name).then(data => data.rows).then(rows => rows[0]),
+        db.getStationRetInfo(name).then(data => data.rows).then(rows => rows[0]),
+        db.getStationDepTop5Info(name).then(data => data.rows),
+        db.getStationRetTop5Info(name).then(data => data.rows),
+    ]);
+
+    res.send(array);
 };
 
 module.exports = getStation;
