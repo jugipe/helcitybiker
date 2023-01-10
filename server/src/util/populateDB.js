f01174799f8fd4d27f9596ac2e75c2e2e97e5b40
jugipe <jukka.pelli@tuni.fi>
2023-01-10 18:21:42 +0200

    Refactor populateDB to be more synchronous so startup chain is more consistent

--- server/src/util/populateDB.js
+++ server/src/util/populateDB.js
@@ -1,103 +1,93 @@
-const fs = require("fs");
+const fs = require("fs")
 const path = require("path");
 const db = require("../db/db");
 const urlParse = require("url");
 const https = require("https");
 
-const populateDB = (dir) => {
-    // names of the folders that should have the data inside
-    const dirs = ["stationdata", "journeydata"];
-    dirs.forEach(folder => {
-        readFiles(dir+folder, folder)
+const populateDB = async(dir) => {
+    // names of the folders that have the data inside
+        const folders = ["stationdata", "journeydata"];
+        const promiseList = folders.map(folder => {readFiles(dir+folder, folder)})
+        return await Promise.all(promiseList).then(() => {return "--Database populated--"});
+}
+
+const readFiles = async(dir, dataType) => {
+    const files = fs.readdirSync(dir);
+
+    if(files.length === 0){
+        if(dataType === "stationdata"){
+            return downloadStationData(dir);
+        } else if (dataType === "journeydata"){
+            return downloadJourneyData(dir);
+        }
+    }
+
+    files.forEach(filename => {
+        return readCSV(path.resolve(dir, filename), dataType);
     });
 }
 
-const readFiles = (dir, dataType) => {
+const readCSV = (filepath, dataType) => {
+    return new Promise(async(res, rej) => {
+        // differentiate the tables with datatype and add them to table 
+        if(dataType === "stationdata"){
+            return db.addCSVtoTable("stations", filepath);
+        }
 
-    // read all the files in a directory
-    fs.readdir(dir, (error, fileNames) => {
-        if(error) throw error;
-
-        // For each filename get the path and extension
-        fileNames.forEach(filename => {
-            const ext = path.parse(filename).ext;
-            const filepath = path.resolve(dir, filename);
-
-            fs.stat(filepath, (error, stat) => {
-                if(error){
-                    throw error;
-                }
-
-                // check that it is a file and not a directory
-                const isFile = stat.isFile();
-
-                // read the if it is a file and has .csv extension
-                if(isFile && ext === ".csv"){
-                    readCSV(filepath, dataType);
-                }
-            });
-        });
-
-        if(fileNames.length === 0){
-            if(dataType === "stationdata"){
-                downloadStationData(dir);
-            } else if (dataType === "journeydata"){
-                downloadJourneyData(dir);
-            }
+        if(dataType === "journeydata"){
+            return db.addCSVtoTable("journeys", filepath);
         }
     });
-}
-
-const readCSV = async(filepath, dataType) => {
-    
-    // differentiate the tables with datatype and add them to table 
-    if(dataType === "stationdata"){
-        await db.addCSVtoTable("stations", filepath);
-    }
-
-    if(dataType === "journeydata"){
-        await db.addCSVtoTable("journeys", filepath);
-    }
     
 }
 
 const downloadStationData = (dir) => {
-    const url = "https://opendata.arcgis.com/datasets/726277c507ef4914b0aec3cbcfcbfafc_0.csv";
-    const file = fs.createWriteStream(dir+"/stations.csv");
-
-    const req = https.get(url, (response) => {
-        response.pipe(file);
-
-        file.on("finish", () => {
-            file.close();
-            console.log("Finished downloading -> "+url);
-            db.addCSVtoTable("stations", file.path)
-        });
-    });
-}
-
-const downloadJourneyData = async(dir) => {
-    const urls = ["https://dev.hsl.fi/citybikes/od-trips-2021/2021-05.csv",
-                  "https://dev.hsl.fi/citybikes/od-trips-2021/2021-06.csv",
-                  "https://dev.hsl.fi/citybikes/od-trips-2021/2021-07.csv"]
-
-    for (const u of urls){
-
-        const parsed = urlParse.parse(u);
-        const file = fs.createWriteStream(dir+"/"+path.basename(parsed.pathname));
-
-        let url = await followURLredirect(u);
+    return new Promise((res, rej) => {
+        const url = "https://opendata.arcgis.com/datasets/726277c507ef4914b0aec3cbcfcbfafc_0.csv";
+        const file = fs.createWriteStream(dir+"/stations.csv");
 
         const req = https.get(url, (response) => {
             response.pipe(file);
 
-            file.on("finish", () => {
+            file.on("finish", async() => {
                 file.close();
-                console.log("Finished downloading -> "+url);
-                db.addCSVtoTable("journeys", file.path);
+                await db.addCSVtoTable("stations", file.path)
+                res("Finished downloading -> "+url);
+            });
+            file.on("error", (err) => {
+                file.close()
+                rej(err.message)
             })
-        })
-    }
+        });
+    })
+}
+
+const downloadJourneyData = (dir) => {
+    return new Promise(async(res, rej) => {
+
+        const urls = ["https://dev.hsl.fi/citybikes/od-trips-2021/2021-05.csv",
+                    "https://dev.hsl.fi/citybikes/od-trips-2021/2021-06.csv",
+                    "https://dev.hsl.fi/citybikes/od-trips-2021/2021-07.csv"]
+
+        for (const u of urls){
+
+            const parsed = urlParse.parse(u);
+            const file = fs.createWriteStream(dir+"/"+path.basename(parsed.pathname));
+
+            let url = await followURLredirect(u);
+
+            const req = https.get(url, (response) => {
+                response.pipe(file);
+
+                file.on("finish", async () => {
+                    file.close();
+                    console.log("Finished downloading -> "+url);
+                    await db.addCSVtoTable("journeys", file.path);
+                    res();
+                })
+            })
+        }
+    });
 }
 
 const followURLredirect = (url) => {
