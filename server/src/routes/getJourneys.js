df5a52a27605b965f3d9046a81aa5ecba3b9977e
jugipe <jukka.pelli@tuni.fi>
2023-01-10 07:55:34 +0200

    Modify the response data to be an object instead of an array, Update tests accordingly

--- server/src/routes/getJourneys.js
+++ server/src/routes/getJourneys.js
@@ -3,11 +3,19 @@ const db = require("../db/db");
 const getJourneys = async (req, res) => {
     const offset = req.query.offset;
     const limit = req.query.limit;
-    const journeys = await Promise.all([
+
+    const array = await Promise.all([
         db.getJourneys(offset, limit).then(data => data.rows),
         db.getJourneyCount().then(data => data.rows).then(rows => rows[0])
-    ]);
-    res.send(journeys);
+    ])
+
+    // Wrap the data to object for easier maintenance in frontend and send the response
+    const wrapped = {
+        journeys: array[0],
+        total: array[1].total
+    }
+
+    res.send(wrapped);
 };
 
 module.exports = getJourneys;
